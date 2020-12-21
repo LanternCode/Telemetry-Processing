065958dcff7c9b4e94ccaa96a1f5794848855d3a
Dominik Michalski <55097755+dominikmch@users.noreply.github.com>
2020-12-21 12:46:26 +0000

    Update create-tables.sql

--- create-tables.sql
+++ create-tables.sql
@@ -4,16 +4,15 @@ GRANT SELECT, INSERT, UPDATE, DELETE, DROP ON coursework.* TO user@localhost IDE
 
 CREATE TABLE `messages` (
   `message_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `switch_01` BOOLEAN NOT NULL,
-  `switch_02` BOOLEAN NOT NULL,
-  `switch_03` BOOLEAN NOT NULL,
-  `switch_04` BOOLEAN NOT NULL,
-  `heater` int(3) NOT NULL,
-  `keypad` int(1) NOT NULL,
-  `fan` ENUM('forward', 'reverse') NOT NULL,
+  `switch_01` BOOLEAN ,
+  `switch_02` BOOLEAN ,
+  `switch_03` BOOLEAN ,
+  `switch_04` BOOLEAN ,
+  `heater` decimal(10) NOT NULL,
+  `keypad` int(1),
+  `fan` varchar(7) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(`message_id`)
-)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 //Inserting
 
