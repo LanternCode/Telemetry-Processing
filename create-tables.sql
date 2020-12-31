7234364e1e69c67e3315fcbba5e3910d8de0ec5f
Dominik Michalski <55097755+dominikmch@users.noreply.github.com>
2020-12-31 10:42:11 +0000

    Update create-tables.sql

--- create-tables.sql
+++ create-tables.sql
@@ -11,17 +11,13 @@ CREATE TABLE `messages` (
   `heater` float(10) ,
   `keypad` int(1),
   `fan` varchar(7) NOT NULL,
+   `unique_message` varchar(10),
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(`message_id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 //Inserting
 
-INSERT INTO messages
-(switch_01, switch_02, switch_03, switch_04, heater, keypad, fan)
-VALUES
-(false, false, false, true , 411, 0, 'reverse'),
-(false, false, false, false , 51, 3, 'forward');
 
 CREATE TABLE users ( 
   id INT NOT NULL AUTO_INCREMENT , 
