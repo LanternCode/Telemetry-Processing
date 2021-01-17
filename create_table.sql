4c96fef4ed7c0154103fa5b3345aa154adfa576c
Adam Machowczyk <P2429405@LEC-ADMIN.dmu.ac.uk>
2021-01-17 16:16:01 +0000

    Added the mysql scripts into the repository

--- create_table.sql
+++ create_table.sql
@@ -1,3 +1,5 @@
+
+
 DROP TABLE IF EXISTS `messages`;
 
 GRANT SELECT, INSERT, UPDATE, DELETE, DROP ON coursework.* TO user@localhost IDENTIFIED BY'password';
@@ -11,25 +13,28 @@ CREATE TABLE `messages` (
   `heater` float(10) ,
   `keypad` int(1),
   `fan` varchar(7) NOT NULL,
-   `unique_message` varchar(10),
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `unique_message` varchar(10),
+  `src_number` varchar(12),
   PRIMARY KEY(`message_id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE users ( 
-  id INT NOT NULL AUTO_INCREMENT , 
-  password VARCHAR(255) NOT NULL , 
-  email VARCHAR(50) NOT NULL , 
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT ,
+  password VARCHAR(255) NOT NULL ,
+  email VARCHAR(50) NOT NULL ,
   passwordresetkey VARCHAR(255),
   role VARCHAR(15) NOT NULL,
   barred BOOLEAN NOT NULL DEFAULT false,
   createdate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)) ENGINE = MyISAM;
-               
-CREATE TABLE activity ( 
-  id INT NOT NULL AUTO_INCREMENT , 
-  userId INT NOT NULL , 
-  activity VARCHAR(50) NOT NULL , 
+
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE activity (
+  id INT NOT NULL AUTO_INCREMENT ,
+  userId INT NOT NULL ,
+  activity VARCHAR(50) NOT NULL ,
   activitydate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  result BOOLEAN NOT NULL, 
+  result BOOLEAN NOT NULL,
   PRIMARY KEY (id)) ENGINE = MyISAM;
